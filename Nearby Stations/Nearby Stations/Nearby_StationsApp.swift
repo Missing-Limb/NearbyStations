6b42380d2b51be4e5cd10da176c6a912d886d06f
Gigi <42325924+g-cqd@users.noreply.github.com>
2023-12-05 11:50:58 +0100

    Initial Commit

---
+++ Nearby Stations/Nearby Stations/Nearby_StationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Nearby_StationsApp.swift
+//  Nearby Stations
+//
+//  Created by Guillaume Coquard on 05/12/23.
+//
+
+import SwiftUI
+
+@main
+struct Nearby_StationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
